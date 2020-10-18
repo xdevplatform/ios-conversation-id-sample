628f74bc42d70c7ceaaea0e5bbd7966e392c6f54
Daniele Bernardi <dbernardi@twitter.com>
2020-10-18 15:09:15 -0700

    Initial Commit

---
+++ Threader/ThreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreaderApp.swift
+//  Threader
+//
+//  Created by Daniele Bernardi on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct ThreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
